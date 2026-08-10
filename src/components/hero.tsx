98e9baac8eae7e0c63a36a6adcefbd52b4d586f7
Ivan Usheff <iusheff@gmail.com>
2026-08-10 16:43:46 -0400

    se agrega cv para descargar

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,8 +4,8 @@ import type { HeroType } from "../types"
 
 export const Hero = (props: HeroType) => {
 
-	const { name, lastName, role, description, links } = props;
-
+	const { name, lastName, role, description, links, cv } = props;
+	const cvurl = cv ? `${import.meta.env.BASE_URL}/cv/${cv}` : `${import.meta.env.BASE_URL}/cv/`;
 
 	const actionsLinks = () => {
 		return links.map((link, index) => {
@@ -17,6 +17,21 @@ export const Hero = (props: HeroType) => {
 				</a>
 			)
 		})
+	}
+
+	const cvDownloaderLink = () => {
+		if (cv) {
+			return (
+				<a 
+					href={cvurl} 
+					className="button primary" 
+					target="_blank"
+					rel="noopener noreferrer"
+					download={cv}>
+					CV
+				</a>
+			)
+		}
 	}
 
 	return (
@@ -44,7 +59,9 @@ export const Hero = (props: HeroType) => {
 					<a href="#projects" className="button primary">
 						Ver proyectos
 					</a>
-					
+
+					{cvDownloaderLink()}
+
 					{actionsLinks()}
 
 				</div>
--- src/types/hero.type.ts
+++ src/types/hero.type.ts
@@ -2,12 +2,14 @@ import type { LinkType } from "./utils";
 
 
 export type HeroLinksType = LinkType;
+export type HeroCVType = `${string}.pdf`;
 
 export type HeroType = {
   name: string;
   lastName: string;
   role: string;
   description: string;
+  cv?: HeroCVType;
   image?: string;
   links: HeroLinksType[];
 }
